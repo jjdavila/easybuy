34e2b3965f0529d95ebee8611f93c9490177f0a0
Miguel Veces <mveces@yahoo.com>
2013-09-08 20:02:19 -0500

    #1 repositorio para el proyecto de easu buy 507 commit numero 1 Signed-off-by:Miguel Veces <mveces@yahoo.com>

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		
 		<!-- Styles -->
 		<link rel="stylesheet" href="css/frenzy-orange.css">
-		
+		<link rel="stylesheet" href="css/easy507.css">
 		<!-- Fav and touch icons -->
 		<link rel="shortcut icon" href="img/ico/favicon.ico">
 
@@ -47,7 +47,27 @@
 						<!-- top nav -->
 						<div class="topnav">
 							<div class="pull-left">
-								<span>Visitante numero: 10349423</span>
+                                  <?php
+                                    session_start();
+								    if (isset($_SESSION["cuenta_paginas"])){ 
+								         	$_SESSION["cuenta_paginas"] = 1; 
+								      }else{ 
+								         	$_SESSION["cuenta_paginas"]=0; 
+								      }
+								      $archivo_contador = "contador/visitas.txt";
+								      $leer = fopen($archivo_contador,"r");
+								      $cuenta = trim(fread($leer,filesize($archivo_contador))); 
+
+								      if ($cuenta != "") $cuenta = $_SESSION["cuenta_paginas"] + $cuenta;
+								      else $cuenta = 1;
+								      @fclose($leer);
+								      $leer = fopen($archivo_contador,"w");
+								      @fputs($leer,$cuenta);
+
+								      @fclose($leer); 
+								      echo '<span class="visitantes">Visitante numero: '.$cuenta.'</span>';
+                                ?>
+								
 							</div>							
 						</div>
 					</div>
@@ -83,7 +103,7 @@
 							<span class="collapse-trigger icon awe-chevron-down"></span>
 						</div>
 						<ul class="first-level dropdown-nav">	
-							<li><a href="quienesomos507.html">Quienes Somos</a></li>
+							<li><a href="quienesomos507.php">Quienes Somos</a></li>
 							<li><a href="comofunciona.html">Como Funciona <span class="icon awe-chevron-down"></span></a>
 								<ul class="second-level">
 								    <li><a href="comofunciona.html">Como Funciona </a></li>
@@ -101,7 +121,7 @@
 					<nav id="main-navigation" class="visible-desktop">
 						<ul class="first-level">
 							<li class="current"><a href="index507.html">Inicio</a></li>
-							<li><a href="quienesomos507.html">Quienes Somos</a></li>
+							<li><a href="quienesomos507.php">Quienes Somos</a></li>
 							<li><a href="comofunciona.html">Como Funciona<span class="icon awe-chevron-down"></span></a>
 								<ul class="second-level">
 								    <li><a href="comofunciona.html">Como Funciona </a></li>
--- quienesomos507.php
+++ quienesomos507.php
@@ -14,6 +14,7 @@
 		
 		<!-- Styles -->
 		<link rel="stylesheet" href="css/frenzy-orange.css">
+		<link rel="stylesheet" href="css/easy507.css">
 		
 		<!-- Fav and touch icons -->
 		<link rel="shortcut icon" href="img/ico/favicon.ico">
@@ -47,7 +48,27 @@
 						<!-- top nav -->
 						<div class="topnav">
 							<div class="pull-left">
-								<span>Visitante numero: 10349423</span>
+								  <?php
+								    session_start();
+                                    if (isset($_SESSION["cuenta_paginas"])){ 
+									   	$_SESSION["cuenta_paginas"] = 0; 
+									}else{ 
+									   	$_SESSION["cuenta_paginas"]=1; 
+									}
+                                    $archivo_contador = "contador/visitas.txt";
+                                    $leer = fopen($archivo_contador,"r");
+                                    $cuenta = trim(fread($leer,filesize($archivo_contador))); 
+
+                                    if ($cuenta != "") $cuenta = $_SESSION["cuenta_paginas"] + $cuenta;
+                                    else $cuenta = 1;
+                                    @fclose($leer);
+                                    $leer = fopen($archivo_contador,"w");
+                                    @fputs($leer,$cuenta);
+
+                                    @fclose($leer); 
+                                    echo '<span class="visitantes">Visitante numero: '.$cuenta.'</span>';
+                                ?>
+								
 							</div>							
 						</div>
 					</div>
@@ -83,8 +104,8 @@
 							<span class="collapse-trigger icon awe-chevron-down"></span>
 						</div>
 						<ul class="first-level dropdown-nav">							
-							<li class="current"><a href="index.html">Inicio</a></li>
-							<li><a href="quienesomos507.html">Quienes Somos</a></li>
+							<li class="current"><a href="index.php">Inicio</a></li>
+							<li><a href="quienesomos507.php">Quienes Somos</a></li>
 							<li><a href="comofunciona.html">Como Funciona <span class="icon awe-chevron-down"></span></a>
 								<ul class="second-level">
 								    <li><a href="comofunciona.html">Como Funciona </a></li>
@@ -101,8 +122,8 @@
 					<!-- este menu que se usa cuando es responsivo lo voy a crear de forma diferente para no hacer el codigo tan largo -->
 					<nav id="main-navigation" class="visible-desktop">
 						<ul class="first-level">
-							<li class="current"><a href="index.html">Inicio</a></li>
-							<li><a href="quienesomos507.html">Quienes Somos</a></li>
+							<li class="current"><a href="index.php">Inicio</a></li>
+							<li><a href="quienesomos507.php">Quienes Somos</a></li>
 							<li><a href="comofunciona.html">Como Funciona <span class="icon awe-chevron-down"></span></a>
 								<ul class="second-level">
 								    <li><a href="comofunciona.html">Como Funciona </a></li>
@@ -212,6 +233,7 @@
 							<div class="separator"></div>
 						</div>
 						<div class="sidebar-content post-widget">
+						
 							<ul>
 								<li class="sidebar-item">
 									<a href="#" class="image-polaroid" title="Title">
